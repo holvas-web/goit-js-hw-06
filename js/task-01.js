25894d4c0f88b747753b262e3b115c13a2eecfe4
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-04-15 18:45:46 +0200

    add script task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,18 @@
-const counter = document.querySelectorAll('.item').length;
-console.log('Кількість категорій: ', counter);
+const categories = document.querySelectorAll('.item')
 
-for (let i = 0; i < counter; i++) {
-    const categories = document.querySelectorAll('.item')[i].querySelector('h2').textContent;
-    console.log('Категорії: ', categories);
-    const categories = document.querySelectorAll('.item')[i].querySelectorAll('li').length;
-    console.log('Елементів: ', categories);
-}
+console.log(`Кількість категорій: ${categories.length}`);
+
+categories.forEach(items => {
+    console.log(`Категорій: ${items.firstElementChild.textContent}`)
+    console.log(`Елементів: ${items.lastElementChild.querySelectorAll('li').length}`)
+})
+
+// const counter = document.querySelectorAll('.item').length;
+// console.log('Кількість категорій: ', counter);
+
+// for (let i = 0; i < counter; i++) {
+//     const categories = document.querySelectorAll('.item')[i].querySelector('h2').textContent;
+//     console.log('Категорії: ', categories);
+//     const categories = document.querySelectorAll('.item')[i].querySelectorAll('li').length;
+//     console.log('Елементів: ', categories);
+// }
