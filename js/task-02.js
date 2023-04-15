d400608fc3e9d15baac3d23e39787263ed5a6562
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-04-15 18:50:21 +0200

    add script task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector('ul');
+const array = [];
+for (let i = 0; i < ingredients.length; i++) {
+    const li = document.createElement("li");
+    li.classList.add('item');
+    li.textContent = ingredients[i];
+    array.push(li);
+
+}
+ul.append(...array);
