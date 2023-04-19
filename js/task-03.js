2a8841f99051734ad4fbc6760415e6811eabeac5
holvas-web <66141432+holvas-web@users.noreply.github.com>
2023-04-19 16:19:40 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,30 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const ul = document.querySelector('li');
+const arr = [];
+for (let i = 0; i < images.length; i++) {
+    const markup = "<li><img src='${ image[i].url }'
+    alt = '${ image[i].alt }' < /li > ";
+    arr.push(markup);
+
+}
+ul.insertAdjacentHTML('beforeend', arr.join(''));
+
+// const list = document.querySelector('.gallery')
+
+// const markup = images
+// 	.map(image => `<li><img class="picture" src=${image.url} width = '400' alt='${image.alt}'></img></li>`)
+// 	.join('')
+
+//   list.insertAdjacentHTML('beforeend', markup)
+//     // const img = document.createElement('img')
+  
+// const pictures = document.querySelectorAll('.picture')
+
+// list.style.display = 'flex';
+// list.style.flexWrap = 'column-reverse'
+// list.style.listStyle = 'none'
+// list.style.gap = '20px'
+// list.style.justifyContent = 'center'
